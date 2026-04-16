1ad36500bc38bfbf82975af36f338a5175c32b1f
Mycline Shareena J <johnpeterkennedy.m@northeastern.edu>
2026-04-16 17:39:39 -0400

    Add files via upload

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,7 +7,6 @@ const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about", label: "About" },
   { href: "#projects", label: "Projects" },
-  { href: "#brand", label: "Brand" },
   { href: "#contact", label: "Contact" },
 ]
 
