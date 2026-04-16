c0cf1128f49c27739bd83d84e2c10acc3c84be78
Mycline Shareena J <johnpeterkennedy.m@northeastern.edu>
2026-04-16 17:33:37 -0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Nav } from "@/components/nav"
 import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
 import { Projects } from "@/components/projects"
+import { BrandAssets } from "@/components/brand-assets"
 import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <BrandAssets />
       <Contact />
       <Footer />
     </main>
